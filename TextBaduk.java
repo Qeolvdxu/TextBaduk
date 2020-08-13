f3aee56076ad855768cb958671dc33f4dd795c45
Qeolvdxu <hve0002@uah.edu>
2020-08-12 19:30:25 -0500

    Added README and place function

--- TextBaduk.java
+++ TextBaduk.java
@@ -4,9 +4,7 @@ class TextBaduk {
 	public static void main(String[] args) {
 		char[] point = new char[100]; 
 		Board.clear(point);
-
-		
-
+		Board.place(point, '0', 6, 7);
 		Board.print(point);
 
 	}
@@ -15,9 +13,11 @@ class Board {
 	public static void print(char[] point) {
 		int line = 0;
 		System.out.print("\u001b[2J");
-		for(int i = 0; i < 9; i++)
+		System.out.printf("   0  1  2  3  4  5  6  7  8  9\n\n");
+		for(int i = 0; i < 10; i++)
 		{
 			line = i * 10;
+			System.out.printf("%d  ",i);
 			for (int j = 0; j < 10; j++)
 			{
 				System.out.printf("%c",point[j+line]);
@@ -26,9 +26,12 @@ class Board {
 				else
 					System.out.printf("\n");
 			}
-			System.out.printf("|  |  |  |  |  |  |  |  |  |\n"); //print 9
+			if (i < 9)
+				System.out.printf("   |  |  |  |  |  |  |  |  |  |\n"); //print 9
 		}
-		System.out.println("+--+--+--+--+--+--+--+--+--+"); //print 10
+	}
+	public static void place(char[] point, char player, int x, int y) {
+		point[(x-1)+((y-1)*10)] = '0';		
 	}
 	public static void clear(char[] point) {
 		Arrays.fill(point, '+');
